d6e2b5c0f9fd448ab8e27d536a6fbba7c2e00984
Odusanya Oluwatobi Funmilayo <tobilobaodusanya2000@gmail.com>
2026-02-06 14:08:09 +0100

    Update 02_case1_region_rankings.sql

--- sql/sql/02_case1_region_rankings.sql
+++ sql/sql/02_case1_region_rankings.sql
@@ -1,13 +1,13 @@
 -- Question 2
 -- What were the top three (3) and bottom three (3) regions in terms of sales?
 
--- TOP 3
+-- TOP 3 Regions by Sales
 SELECT 
   TOP 3 Region, sales
 FROM [KMS Order Table]
 ORDER BY Sales DESC
 
--- BOTTOM 3
+-- BOTTOM 3 Regions by Sales
 SELECT 
   TOP 3 Region, sales
 FROM [KMS Order Table]
