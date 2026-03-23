66a9ef068d93de9ef09b4860a24d57fcbdd9c857
Odusanya Oluwatobi Funmilayo <tobilobaodusanya2000@gmail.com>
2026-03-23 08:18:52 +0100

    Update 08_top_corporate_customer_orders.sql

--- sql/08_top_corporate_customer_orders.sql
+++ sql/08_top_corporate_customer_orders.sql
@@ -1,7 +1,7 @@
 -- Qiestion 8
 -- Which Corporate Customer placed the most number of orders in 2009 – 2012?
 
-Select top 50 Customer_Name, customer_segment, count(distinct Order_ID) as Frequency_of_Orders
+Select top 1 Customer_Name, customer_segment, count(distinct Order_ID) as Frequency_of_Orders
 from [KMS Order Table]
 where Customer_Segment = 'Corporate'
 Group by Customer_Name, customer_segment
