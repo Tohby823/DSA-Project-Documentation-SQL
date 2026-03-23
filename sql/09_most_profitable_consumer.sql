e8587b6f0de93863f1bb4eab11bb32bd6807df6b
Odusanya Oluwatobi Funmilayo <tobilobaodusanya2000@gmail.com>
2026-03-23 08:19:23 +0100

    Update 09_most_profitable_consumer.sql

--- sql/09_most_profitable_consumer.sql
+++ sql/09_most_profitable_consumer.sql
@@ -1,7 +1,8 @@
 -- Question 9
 -- Which consumer customer was the most profitable one?
 
-Select top 1 Customer_Name,Customer_Segment, Order_Quantity, Product_Category, Profit
+Select top 1 Customer_Name,Customer_Segment, Sum(Profit) as Total_Profit
 from [KMS Order Table]
 where Customer_Segment = 'Consumer'
-order by Profit desc
+group by Customer_Name, Customer_Segment
+order by Total_Profit desc
