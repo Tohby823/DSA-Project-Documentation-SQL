23d3fa09a1fb979d63587d7f651926ab80ce4a2a
Odusanya Oluwatobi Funmilayo <tobilobaodusanya2000@gmail.com>
2026-03-23 08:12:35 +0100

    Update 07_top_small_business_customer.sql

--- sql/07_top_small_business_customer.sql
+++ sql/07_top_small_business_customer.sql
@@ -1,8 +1,8 @@
 -- Question 7
 -- Which small business customer had the highest sales?
 
-Select 
-  top 1 Customer_Name, customer_segment, Sales
-from [KMS Order Table]
-where Customer_Segment = 'Small Business'
-order by Sales desc
+SELECT top 1 Customer_Name, Customer_Segment, SUM(Sales) AS Total_Sales
+FROM [KMS Order Table]
+WHERE Customer_Segment = 'Small Business'
+GROUP BY Customer_Name, Customer_Segment
+ORDER BY Total_Sales desc
