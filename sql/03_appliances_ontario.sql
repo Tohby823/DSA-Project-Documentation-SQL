c19a20b06a7fc9e3ac098c0e73ae9863524d446d
Odusanya Oluwatobi Funmilayo <tobilobaodusanya2000@gmail.com>
2026-03-23 08:07:38 +0100

    Update 03_appliances_ontario.sql

--- sql/03_appliances_ontario.sql
+++ sql/03_appliances_ontario.sql
@@ -1,8 +1,8 @@
 -- Question 1
 -- What were the total sales of appliances in Ontario?
 
-Select 
+SELECT
   Province, product_sub_category, sum(sales) as REVENUE
 FROM [KMS Order Table]
-Where Province = 'Ontario' and product_sub_category = 'Appliances'
-group by Province, product_sub_category
+WHERE Province = 'Ontario' and product_sub_category = 'Appliances'
+GROUP BY Province, product_sub_category
